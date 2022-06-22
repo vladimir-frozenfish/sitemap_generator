24c9996c34b8b1f33df8389839fbe5ae7f23ca05
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-06-22 23:54:13 +0300

    queue_urls.py - проход по всем ссылкам и выведение в терминал Необходимо: - описать класс времени и сделать определение сколько осуществляется обход - запись результатов в файл

--- queue_urls.py
+++ queue_urls.py
@@ -4,19 +4,46 @@ from urls_from_one_class import Url
 
 
 def main():
-    url_main = Url('http://frozenfish.pythonanywhere.com/')
-    urls_deque = deque()
+    urls_deque = deque()    # очередь для добавления классов страниц
+    links_set = set()       # множество ссылок для сравнения
+
+    url_main = Url('https://thecode.media')
+    # url_main = Url('http://crawler-test.com/')
+    # url_main = Url('https://github.com/')
+    # url_main = Url('http://frozenfish.pythonanywhere.com')
+    # url_main = Url('http://frozenfish.site')
+
     urls_deque.append(url_main)
+    links_set.add(url_main.url)
 
     count = 0
     while count < len(urls_deque):
+        # print(urls_deque)
         current_url = urls_deque[count]
+
+        """если текущий url родственен главному, то получаем все ссылки на текущей странице, 
+        после они добавятся в пустой атрибут .links,
+        если текущий url - сторонний, то список ссылок не получаем, он будет пуст"""
+        if current_url.url.startswith(url_main.url):
+            current_url.get_links()
         # проходим по всем ссылкам текущей страницы, если их нету в очереди добавляем как классы в очередь
         for link in current_url.links:
-            pass
+            if link not in links_set:
+                urls_deque.append(Url(link, current_url))
+                links_set.add(link)
 
-    print(urls_deque)
+        count += 1
+        print(f'Обработано страниц - {count}, Обратотанная страницы - {current_url.url}')
 
+    # print(len(urls_deque))
+    # print(urls_deque)
+    print(f'Всего ссылок - {len(urls_deque)}')
+    # print(links_set)
+
+    count = 1
+    for url in urls_deque:
+        print(f'{count}: {url}')
+        count += 1
 
 if __name__ == '__main__':
     main()
--- urls_from_one_class.py
+++ urls_from_one_class.py
@@ -11,24 +11,24 @@ from urllib import parse
 class Url:
     """класс Url"""
     def __init__(self, url, parent=None):
-        self.url = url
+        self.url = url if url.endswith('/') else url + '/'
         self.parent = parent
-        self.links = self.get_links()
+        # self.links = self.get_links()
+        self.links = set()
 
     def __str__(self):
         return self.url if self.parent is None else f'{self.parent} -> {self.url}'
 
     def __repr__(self):
-        return self.url if self.parent is None else f'{self.parent} -> {self.url}'
+        # return self.url if self.parent is None else f'{self.parent} -> {self.url}'
+        return self.url
 
     def __len__(self):
         return len(self.links)
 
-    # def __eq__(self, other):
-
-
-
     def get_html_text(self):
+        if self.url.startswith('mailto:') or self.url.startswith('tel:'):
+            return ''
         response = requests.get(self.url)
         response.encoding = 'utf-8'
         return response.text
@@ -40,10 +40,11 @@ class Url:
         return tag_a[index_href + 6: end_url]
 
     def get_links(self):
-        """возвращает множество (set) ссылок из html-текста"""
+        """возвращает множество (set) ссылок из html-текста и помещает
+        в атрибут класса links - изначально это множетво пустое"""
         html_text = self.get_html_text()
 
-        links = set()
+        # links = set()
         start_find = 0
 
         while True:
@@ -55,30 +56,41 @@ class Url:
             # получение ссылки, очищенной от тэгов
             href = self.get_link_from_tag(html_text[index_a_start:index_a_end + 4])
 
+            # если ссылка не равна главной, то
             # добавление ссылки во множество, если ссылка относительная или с параметрами, то соединяется с базовой
-            links.add(parse.urljoin(self.url, href))
+            # links.add(parse.urljoin(self.url, href))
+            link = parse.urljoin(self.url, href)
+            if link != self.url:
+                self.links.add(link)
 
             start_find = index_a_end + 4
 
-        return links
+        return self.links
 
     def print_links(self):
+        print(self.url)
         for link in self.links:
             print(link)
 
 
 if __name__ == '__main__':
+    '''
     url_yatube = Url('http://frozenfish.pythonanywhere.com/')
-    print(url_yatube)
+    url_yatube.get_links()
     url_yatube.print_links()
+    '''
 
+    '''
+    url_yatube_1 = Url('http://frozenfish.pythonanywhere.com/?page=3')
+    url_yatube_1.get_links()
+    url_yatube_1.print_links()
+    '''
 
-    print(url_yatube.url == 'http://frozenfish.pythonanywhere.com/')
+    url_google = Url('https://www.kinopoisk.ru/')
 
+    url_google.get_links()
+    url_google.print_links()
 
-
-    url_yatube_next = Url('http://frozenfish.pythonanywhere.com/group/russian/')
-    url_yatube_next.print_links()
 
 
 
